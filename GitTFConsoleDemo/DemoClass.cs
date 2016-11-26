959233efc20e9d157d7e7af5ffafb2ba2ffd4828
jscotton <j_scotton@yahoo.com>
2016-11-26 03:30:10 -0500

    Initial project commit

---
+++ GitTFConsoleDemo/DemoClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTFConsoleDemo
+{
+    class DemoClass
+    {
+        //Additional changes checked into TFS
+
+        //More changes commited to Git
+
+        //One more change
+    }
+}
---
+++ GitTFConsoleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTFConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This was checked in directly to TFS");
+
+            Console.WriteLine("Change made in local Git repository");
+        }
+    }
+}
